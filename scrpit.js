6ed9808afea2effabf1e3b4534edbbb9096134a2
KoliRamonee <161612645+KoliRamonee@users.noreply.github.com>
2024-03-05 19:43:16 -0300

    Update scrpit.js

--- scrpit.js
+++ scrpit.js
@@ -1,14 +1,6 @@
 const campo_texto = document.querySelector("#texto-encriptado");
 const campo_mensaje = document.querySelector("#campo-mensaje");
 
-const matriz_code=[
-    ["e", "enter"],
-    ["i", "imes"],
-    ["a", "ai"],
-    ["o", "ober"],
-    ["u", "ufat"],
-];
-
 function btnEncriptar(){
     const textoEncriptado =encriptar(campo_texto.value);
     campo_mensaje.value = textoEncriptado;
